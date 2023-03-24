e48931735222182d74b37ca2f07d67dea1046520
김보연 <122425985+Boyeon-Kimm@users.noreply.github.com>
2023-03-25 02:07:11 +0900

    [Silver III] Title: N과 M (8), Time: 112 ms, Memory: 14524 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static int[] num, res;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		num = new int[N];
+		res = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		perm(0, 0);
+		System.out.println(sb);
+	}
+	private static void perm(int idx, int s) {
+		if(idx == M) {
+			for(int i = 0; i < M; i++) {
+				sb.append(res[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i = s; i < N; i++) {
+			res[idx] = num[i];	
+			perm(idx + 1, i);
+		}
+		
+	}
+}
