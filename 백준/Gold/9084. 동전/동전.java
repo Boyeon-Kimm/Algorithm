a31224bee363dbae73f465e9b29ceb2ed82d2f99
김보연 <122425985+Boyeon-Kimm@users.noreply.github.com>
2023-04-12 19:54:01 +0900

    [Gold V] Title: 동전, Time: 84 ms, Memory: 11860 KB -BaekjoonHub

---
+++ 백준/Gold/9084. 동전/동전.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			
+			int[] coins = new int[N + 1];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for(int i = 1; i <= N; i++) {
+				coins[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int M = Integer.parseInt(br.readLine());
+			
+			int[] dp = new int[M + 1];
+			for(int i = 1; i <= N; i++) {
+				for(int j = 1; j <= M; j++) {
+					if(j - coins[i] > 0) {
+						dp[j] = dp[j] + dp[j - coins[i]];
+					} else if(j - coins[i] == 0) {
+						dp[j]++;
+					}
+				}
+			}
+			sb.append(dp[M] + "\n");
+		}
+		System.out.println(sb);
+	}
+}
