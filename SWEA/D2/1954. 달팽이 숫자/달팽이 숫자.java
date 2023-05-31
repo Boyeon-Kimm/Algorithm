35604d824e3aaae5228f423a19753547fcf411d1
김보연 <122425985+Boyeon-Kimm@users.noreply.github.com>
2023-05-31 11:50:58 +0900

    [D2] Title: 달팽이 숫자, Time: 166 ms, Memory: 22,884 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,40 +1,33 @@
-import java.util.Arrays;
 import java.util.Scanner;
-import java.io.FileInputStream;
 
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
+public class Solution {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		int[] dr = {0, 1, 0, -1};
-		int[] dc = {1, 0, -1, 0};
+		int[] nr = {0, 1, 0, -1};
+		int[] nc = {1, 0, -1, 0};
 		
 		for(int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt();
 			int[][] snail = new int[N][N];
-			int x = 0;
-			int y = 0;
-			int d = 0;
-            
+			int cr = 0, cc = 0, d = 0; 
+			
 			for(int i = 1; i <= N * N; i++) {
-				snail[x][y] = i;
-				x += dr[d];
-				y += dc[d];
-                
-				if(x < 0 || x >= N || y < 0 || y >= N || snail[x][y] > 0) {
-					x -= dr[d];
-					y -= dc[d];
-					d = ( d + 1 ) % 4;
-					x += dr[d];
-					y += dc[d];	
+				snail[cr][cc] = i; 
+				int x = cr + nr[d];
+				int y = cc + nc[d];
+				
+				if(x < 0 || x >= N || y < 0 || y >= N || snail[x][y] != 0) {
+					d = (d + 1) % 4;
 				}
+				cr += nr[d];
+				cc += nc[d];
 			}
+			
 			System.out.println("#" + tc);
 			for(int i = 0; i < N; i++) {
 				for(int j = 0; j < N; j++) {
-					System.out.print(snail[i][j] + " ");
+					System.out.printf("%d ", snail[i][j]);
 				}
 				System.out.println();
 			}
