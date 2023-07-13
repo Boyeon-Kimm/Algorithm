741eccf83a4a1367f4b1be7266c13e3a2157808b
김보연 <122425985+Boyeon-Kimm@users.noreply.github.com>
2023-07-13 17:44:40 +0900

    [Bronze V] Title: A+B - 8, Time: 84 ms, Memory: 11620 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			sb.append("Case #").append(tc).append(": ");
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(a).append(" + ").append(b).append(" = ").append(a + b).append("\n");
+		}
+		System.out.println(sb);
+	}
+}
