5807703974356d522e90d806b681036062fd3749
김보연 <122425985+Boyeon-Kimm@users.noreply.github.com>
2024-03-15 13:18:12 +0900

    [Silver II] Title: 약수의 합 2, Time: 116 ms, Memory: 13004 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        long sum = 0;
+
+        for(int i = 1; i <= N; i++) {
+            sum += (N / i) * i;
+        }
+        System.out.println(sum);
+    }
+}
