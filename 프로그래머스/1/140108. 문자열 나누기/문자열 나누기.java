1dfe768c3781dff13462663eb5ab6218d0f99401
김보연 <122425985+Boyeon-Kimm@users.noreply.github.com>
2024-03-24 16:05:29 +0900

    [level 1] Title: 문자열 나누기, Time: 0.64 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        char x = s.charAt(0);
+        int same = 0;
+        int diff = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(same == diff) {
+                x = s.charAt(i);
+                answer++;
+            }
+            if(s.charAt(i) == x) same++;
+            else diff++;
+        }
+        return answer;
+    }
+}
