07eb92b2d6f9b5a07197597f7b781bd3a261ee13
김보연 <122425985+Boyeon-Kimm@users.noreply.github.com>
2024-03-25 15:35:08 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.35 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+        
+        answer = answer.replaceAll("[^-_.a-z0-9]", ""); // 2단계
+        answer = answer.replaceAll("[.]{2,}", "."); // 3단계
+        answer = answer.replaceAll("^[.]|[.]$", ""); // 4단계
+        
+        // 5단계
+        if(answer.equals("")) answer += "a";
+        
+        // 6단계
+        if(answer.length() >= 16) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$", "");
+        }
+        
+        // 7단계
+        if(answer.length() <= 2) {
+            while(answer.length() < 3) answer += answer.charAt(answer.length() - 1);
+        }
+        
+        return answer;
+    }
+}
